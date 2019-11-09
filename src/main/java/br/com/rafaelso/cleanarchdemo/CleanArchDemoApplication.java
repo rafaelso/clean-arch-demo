a5aace8a3d559f7c508658812120c46a74ce6518
Rafael Santana <rafaelstnoliveira@gmail.com>
2019-11-09 08:55:00 -0300

    Setup

---
+++ src/main/java/br/com/rafaelso/cleanarchdemo/CleanArchDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelso.cleanarchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchDemoApplication.class, args);
+	}
+
+}
